760b214641853d3e23981007c2202acb467f8339
FranCavelli <FranCavelli@hotmail.com>
2024-08-10 02:10:35 -0300

    ✍Styles + 📰Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://FranCavelli.github.io',
+  base: 'LeagueStatus',
   integrations: [tailwind()],
   output: "server",
   adapter: node({
