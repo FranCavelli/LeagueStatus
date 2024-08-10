d3ad862e6b3c08f0a180f694ce9e39203f5eae9d
FranCavelli <FranCavelli@hotmail.com>
2024-08-10 19:11:57 -0300

    🚀 Update astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://FranCavelli.github.io',
   base: '/LeagueStatus/',
   integrations: [tailwind()],
-  output: 'server',
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: 'static',
 });
