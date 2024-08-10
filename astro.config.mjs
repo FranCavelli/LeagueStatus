58f0f2231c7fc342d6fdc1b90c11d890bbed70f1
FranCavelli <FranCavelli@hotmail.com>
2024-08-10 13:44:00 -0300

    🚀 Update astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://FranCavelli.github.io',
