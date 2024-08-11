dc2aea1c4036ddd9039224c0ab33e22fb0aad848
FranCavelli <FranCavelli@hotmail.com>
2024-08-10 21:59:25 -0300

    🐹 Update vercel add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://FranCavelli.github.io',
   base: '/LeagueStatus/',
   integrations: [tailwind()],
   output: 'static',
+  adapter: vercel()
 });
